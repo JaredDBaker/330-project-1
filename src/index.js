3c8239848fecbf3954bb7f8659aa3cc6d4de578b
Jared Baker <jaredbaker2018@gmail.com>
2020-09-17 12:04:16 -0400

    Prototype done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@
 
     let ctx; 
     const canvasWidth = 640, canvasHeight = 480;
+    let divergence = 147.5;
+    let space = 2, size = 2;
+    let steps = 140;
+    let xDis = 0, yDis = 0;
+    let pause = true;
+    let drawTrail = true;
+    let currentMover = "line";
+    let counter = 0;
 
     function init(){
         console.log("page loaded!");
@@ -14,8 +22,77 @@
         canvas.width = canvasWidth;
         canvas.height = canvasHeight;
 
+        canvas.onclick = canvasClicked;
+
         ctx = canvas.getContext('2d');
         ctx.fillRect(0,0,canvasWidth, canvasHeight);
+        setupUI();
+        jdbLIB.drawPhyllotaxis(ctx, canvasWidth/2 + xDis, canvasHeight/2 + yDis, divergence, space, size, steps);
+        playing();
     }
 
+    function setupUI(){
+        document.querySelector("#trailCB").onchange = function(){
+            drawTrail = !drawTrail;
+        }
+        document.querySelector("#playButton").onclick = function(){
+            if(pause == false) return;
+            pause = false;
+            playing();
+        }
+        document.querySelector("#pauseButton").onclick = function(){
+            pause = true;
+        }
+
+        document.querySelector("#chooserMover").onchange = function(){
+            currentMover= this.value;
+        }
+
+        document.querySelector("#resetButton").onclick = function(){
+            jdbLIB.cls(ctx, canvasWidth, canvasHeight);
+            xDis = 0;
+            yDis = 0;
+            counter = 0;
+            jdbLIB.drawPhyllotaxis(ctx, canvasWidth/2 + xDis, canvasHeight/2 + yDis, divergence, space, size, steps);
+        }
+
+    }
+
+    function canvasClicked(e){
+        let rect = e.target.getBoundingClientRect();
+        let mouseX = e.clientX - rect.x;
+        let mouseY = e.clientY - rect.y;
+        console.log(mouseX,mouseY);
+
+        jdbLIB.drawPhyllotaxis(ctx, mouseX, mouseY, divergence, space, size, steps);
+
+    }
+
+    function playing(){
+        if(pause) return;
+        requestAnimationFrame(playing);
+
+        if(drawTrail == true){
+            ctx.globalAlpha = 2/30;
+            jdbLIB.cls(ctx, canvasWidth, canvasHeight);
+            ctx.globalAlpha = 1;
+        }
+        else{
+            jdbLIB.cls(ctx, canvasWidth, canvasHeight);
+        } 
+        jdbLIB.drawPhyllotaxis(ctx, canvasWidth/2 + xDis, canvasHeight/2 + yDis, divergence, space, size, steps);
+
+        if(currentMover == "line"){
+            xDis++;
+        }
+        else if(currentMover = "sine"){
+            xDis++;
+            counter += .1;
+            yDis =  Math.sin(counter) * 100;
+        }
+
+
+        if(xDis > canvasWidth) xDis = -canvasWidth/2;
+        //yDis++;
+    }
 })();
--- src/jdbLIB.js
+++ src/jdbLIB.js
@@ -1,7 +1,43 @@
 (function(){
     "use strict";
     const jdbLIB = {
-    
+        drawPhyllotaxis(ctx, centerX, centerY, divergence, space, size, steps){
+            for(let n = 0; n < steps; n++){
+                let a = n * this.dtr(divergence);
+                let r = space * Math.sqrt(n);
+
+                let x = r * Math.cos(a) + centerX;
+                let y = r * Math.sin(a) + centerY;
+
+                let aDegrees = (n * divergence) % 256;
+                let color = `rgb(${aDegrees},20,50)`;
+
+                this.drawCircle(ctx, x, y, size, color);
+                space += .001;
+                size += .001;
+            }
+        },
+
+        dtr(degrees){
+            return degrees * (Math.PI/180);
+        },
+
+        drawCircle(ctx,x,y,radius,color){
+            ctx.save();
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.arc(x,y,radius,0,Math.PI * 2);
+            ctx.closePath();
+            ctx.fill();
+            ctx.restore();
+        },
+
+        cls(ctx, canvasWidth, canvasHeight){
+            //ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+            ctx.fillStyle = "black";
+            ctx.fillRect(0,0,canvasWidth, canvasHeight);
+        }
+
     };
 
     window["jdbLIB"] = jdbLIB;
